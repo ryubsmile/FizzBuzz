57e8e0d8e8c043ea10c1b82cc919fc9038238f2f
Jaehyuk Ryu <ryubsmile@gmail.com>
2025-09-09 12:37:43 -0400

    add reduce method

---
+++ src/lab1/Reduce.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class Reduce {
+    public static void main(String[] args) {
+        int n = reduce(100);
+        System.out.println(n);
+    }
+
+    public static int reduce(int n) {
+        int steps = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n--;
+            }
+            steps++;
+        }
+        return steps;
+    }
+}
